1f32c1c70410a2ea0af2c327af664a75cd8d17b6
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2020-01-23 11:32:26 +0100

    Created a process.env PORT for heroku

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.use(express.json({ extended: false }));
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
 
-const PORT = 5000;
+const PORT = 5000 || process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
