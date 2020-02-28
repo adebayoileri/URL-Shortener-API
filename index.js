9f4c2ea8d40fbc27f2cb136936831e16b2778f00
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2020-02-28 15:42:32 +0100

    PORT Update

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.use(express.json({ extended: false }));
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
