37fb552c7d16d5706aa8411eea08867c1b3f7a51
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2020-06-08 16:21:58 +0100

    fix:(dupliate ids and copy script)

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,31 @@
+urlForm.addEventListener('click',async() =>{
+    let longUrl = document.getElementById('longurl').value;
+    var shortUrldiv = document.querySelector('#link-list');
+    // console.log(longUrl);
+
+   await fetch('https://urlcuttr.herokuapp.com/api/url/shorten',{
+        method : "POST",
+        headers: {
+            "Content-Type":"application/json",
+            "Accept":"application/json"
+        },
+        body: JSON.stringify({longUrl:longUrl})
+    })
+    .then(response => response.json())
+    .then(data => {
+        // console.log(data)
+        shortUrldiv.innerHTML +=`<div class="link">
+        <span>Long URL</span>
+        <p>${data.longUrl}</p>
+        <span>Shorten Link</span>
+        <input type="text" value=${data.shortUrl} id=${data.urlCode}/>
+        <button id="copyBtn" onclick="copyText(${data.urlCode})" class="btn-click">Copy</button>
+       </div>`;
+       longUrl = ""
+    })
+    .catch(err => console.log(err));
+});
+
 function copyText(link) {
     /* Get the text field */
     var copyText = document.getElementById(link);
@@ -14,29 +42,3 @@ function copyText(link) {
     alert("Copied");
   
   }
-
-urlForm.addEventListener('click',async() =>{
-    const longUrl = document.getElementById('longurl').value;
-    var shortUrldiv = document.querySelector('#link-list');
-    // console.log(longUrl);
-
-   await fetch('https://urlcuttr.herokuapp.com/api/url/shorten',{
-        method : "POST",
-        headers: {
-            "Content-Type":"application/json",
-            "Accept":"application/json"
-        },
-        body: JSON.stringify({longUrl:longUrl})
-    })
-    .then(response => response.json())
-    .then(data => {
-        // console.log(data)
-        shortUrldiv.innerHTML +=` <div class="link">
-        <span>Long URL</span>
-        <p>${data.longUrl}</p>
-        <span>Shorten Link</span>
-        <input type="text" value=${data.shortUrl} id=${data.urlCode} disabled="true"/>
-        <button id="copyBtn" onclick=${copyText(data.urlCode)} class="btn-click">Copy</button>
-       </div>`})
-    .catch(err => console.log(err));
-});
